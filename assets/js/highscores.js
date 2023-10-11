6421344358140f7dadc2ca233ace4833d3170e35
yso <baptisteguillaume5@gmail.com>
2023-10-11 21:50:48 +0200

    page highscore etc

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -2,6 +2,7 @@ const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 highScoresList.innerHTML = highScores
+  .sort((a, b) => b.score.slice(0,3) - a.score.slice(0,3))
   .map(score => {
     return `<li class="high-score"><b>${score.name}</b> : ${score.score}</li>`;
   })
